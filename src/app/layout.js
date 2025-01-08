8c1a361ffbc6975d4e111a4c4505dbdf27ba3dc4
Maheshna369 <maheshnayak9639@gmail.com>
2025-01-08 16:59:31 +0530

    last and final commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full w-full">
       <head>
+      <link rel="icon" href="/free.png" type="image/png" />
         <script
           src="https://checkout.razorpay.com/v1/checkout.js"
           async
